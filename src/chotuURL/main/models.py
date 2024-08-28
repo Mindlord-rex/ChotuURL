b06739f3db8a33430aa0023db5e582a18b952a18
Manish Kumar <manish123hello@gmail.com>
2024-08-28 23:40:19 +0530

    Model created

--- src/chotuURL/main/models.py
+++ src/chotuURL/main/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class ShortURL(models.Model):
+    created = models.DateTimeField(auto_not_add=True)
+    
+    times_followed = models.PositiveIntegerField(default=0)
+    
+    org_url = models.URLField()
+    
+    short_url = models.CharField(max_length=15, unique=True, blank=True)
+    
+    class Meta:
+        ordering = ["-created"]
+        
+    def __str__(self):
+        return f'{self.org_url} to {self.short_url}'
