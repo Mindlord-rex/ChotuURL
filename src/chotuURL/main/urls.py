f5758b454b0ef70f527f07220c78fd97a5854688
Manish Kumar <manish123hello@gmail.com>
2024-08-30 19:29:43 +0530

    updated views and url

--- src/chotuURL/main/urls.py
+++ src/chotuURL/main/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import home_view
+from .views import home_view, redirect_view
 
 app = 'main'
 
 urlpatterns = [
     path('', home_view, name='home'),
+    path('<str:short_part>/', redirect_view, name='redirect')
 ]
--- src/chotuURL/main/views.py
+++ src/chotuURL/main/views.py
@@ -1,6 +1,48 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-# Create your views here.
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+
+from .models import ShortURL
+from .forms import ShortURLForm
 
 def home_view(request):
-    return HttpResponse('Hello World')
+    
+    template = "main/templates/home.html"
+    
+    context = {}
+    
+    context['form'] = ShortURLForm()
+    
+    if request.method == 'GET':
+        return render(request, template, context)
+        
+    elif request.method == 'POST':
+        
+        form = ShortURLForm(request.POST)
+        
+        if form.is_valid():
+            short_obj = form.save()
+            
+            new_url = request.build_absolute_uri('/') + short_obj.short_url
+            org_url = short_obj.org_url
+            
+            context['new_url'] = new_url
+            context['org_url'] = org_url
+            
+            return render(request, template, context)
+        
+    context['error'] = form.errors
+    
+    return render(request, template, context)
+
+def redirect_view(request, short_part):
+    try:
+        shorturl = ShortURL.objects.get(short_url=short_part)
+        
+        shorturl.times_followed += 1
+        
+        shorturl.save()
+        
+        return HttpResponseRedirect(shorturl.org_url)
+    
+    except:
+        raise Http404("URL does not exist")
